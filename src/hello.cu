b54f40c8203d50598a672350190addcea5aaddf9
neoblizz <osama94@gmail.com>
2022-02-27 17:06:04 -0800

    📮 simple executable example.

---
+++ src/hello.cu
@@ -0,0 +1,18 @@
+/**
+ * @file my_app.cu
+ * @author Muhammad Osama (mosama@ucdavis.edu)
+ * @brief
+ * @version 0.1
+ * @date 2022-02-27
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include <gunrock/util/print.hxx>
+#include <iostream>
+
+int main(int argc, char** argv) {
+  using namespace gunrock;
+  std::cout << "Hello, World!" << std::endl;
+}
