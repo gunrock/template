168a74a77eeedb144371819f76621927e5a28896
Muhammad Osama <osama94@gmail.com>
2022-09-18 22:36:48 -0700

    Update FetchEssentials.cmake

--- cmake/FetchEssentials.cmake
+++ cmake/FetchEssentials.cmake
@@ -2,7 +2,7 @@ include(FetchContent)
 set(FETCHCONTENT_QUIET ON)
 
 message(STATUS "Cloning External Project: Essentials")
-get_filename_component(FC_BASE "../externals"
+get_filename_component(FC_BASE "${PROJECT_SOURCE_DIR}/externals"
                 REALPATH BASE_DIR "${CMAKE_BINARY_DIR}")
 set(FETCHCONTENT_BASE_DIR ${FC_BASE})
 
@@ -11,6 +11,7 @@ FetchContent_Declare(
     GIT_REPOSITORY https://github.com/gunrock/essentials.git
     GIT_TAG        master
 )
+
 FetchContent_MakeAvailable(essentials)
 set(ESSENTIALS_INCLUDE_DIR 
     ${FETCHCONTENT_BASE_DIR}/essentials-src/include)
